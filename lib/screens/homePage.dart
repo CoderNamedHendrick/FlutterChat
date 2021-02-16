f906e2aab194fed170dc742fe3dde827b638e2ad
Hendrick <sebastinesoacatp@gmail.com>
2021-02-16 17:05:38 +0100

    Adding the scrollview widget to the homepage

--- lib/screens/homePage.dart
+++ lib/screens/homePage.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
 
+import 'chatPage.dart';
+
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text("Chat"),
-        ),
-      ),
+      body: ChatPage(),
       bottomNavigationBar: BottomNavigationBar(
         selectedItemColor: Colors.red,
         unselectedItemColor: Colors.grey.shade600,
